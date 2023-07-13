59628ed28efd9575d1e0170d8d78f4abcd93ec62
Alison Sav <alisa.zarytska@troops-ext.fr>
2023-07-13 17:57:41 +0200

    add bee

--- bee/tasks.py
+++ bee/tasks.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 from celery import shared_task
 from django.core.mail import send_mail
-from django.template import loader
 
 from bee.models import Quote, Author
 from core import settings
@@ -12,13 +11,20 @@ from core import settings
 def create_news():
     index_link = f"https://quotes.toscrape.com"
     page = 1
+    quotes_count = 0
+    quote_dict = {}
     while True:
-        quotes_count = 0
-        quote_dict = {}
         link = f"https://quotes.toscrape.com/page/{page}"
         r = requests.get(link)
         soup = BeautifulSoup(r.text, "html.parser")
         quotes = soup.findAll("div", class_="quote")
+        if not soup("span", class_="text"):
+            send_mail("No quotes",
+                      "O no! You are have not any quotes",
+                      settings.NOREPLY_EMAIL,
+                      ["ohoho@ho.com"],
+                      fail_silently=False)
+            return
         for quote in quotes:
             text = quote.find("span", class_="text").text
             author = quote.find("small", class_="author").text
@@ -37,19 +43,9 @@ def create_news():
             if not Quote.objects.filter(quote=text).exists():
                 quote_dict[text] = author.id
                 quotes_count += 1
-                print(quote_dict)
                 if quotes_count == 5:
+                    print(quote_dict)
                     Quote.objects.bulk_create(
                         Quote(quote=text, author_id=author) for text, author in quote_dict.items())
-                    break
-                if quotes_count == 0:
-                    send_mail("No quotes",
-                      loader.render_to_string("mac/template_email.html",
-                                              {"message": "O no! You are have not any quotes"}),
-                      settings.NOREPLY_EMAIL,
-                      ["ohoho@ho.com"],
-                      fail_silently=False)
-                    break
-
+                    return
         page += 1
-
