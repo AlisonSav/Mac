7ba4dfe393dbe75a0ccce75647b5de4319d68b7e
Alison Sav <alisa.zarytska@troops-ext.fr>
2023-07-13 19:38:31 +0200

    add bee

--- bee/admin.py
+++ bee/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from bee.models import Quote, Author
+
+
+@admin.register(Quote)
+class QuoteAdmin(admin.ModelAdmin):
+    list_display = ("quote", "author")
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("name", "born_date", "born_loc", "about")
